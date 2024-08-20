a9d72945de799026211a46d9eaaa9048b55136b1
ParasPandey019 <paraspandey019@gmail.com>
2024-08-20 21:24:06 +0530

    changes made to intro.js

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,6 +1,8 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
+
 const Intro = () => {
   const [greet, setGreet] = useState({ name: "NAMASTE", breakAt: "2.7em" });
+  
   const greets = [
     { name: "NAMASTE", breakAt: "2.7em" },
     { name: "HELLO", breakAt: "2em" },
@@ -8,19 +10,28 @@ const Intro = () => {
     { name: "BONJOUR", breakAt: "2.8em" },
     { name: "HOWDY", breakAt: "2.5em" },
   ];
-  function randomGreet() {
-    const randNum = Math.floor(Math.random() * 5);
+
+  const randomGreet = useCallback(() => {
+    const randNum = Math.floor(Math.random() * greets.length);
     return greets[randNum];
-  }
+  }, [greets]);
+
   useEffect(() => {
     const greetText = document.querySelector("#intro-text");
-    greetText.addEventListener("click", () => {
-      setGreet(randomGreet);
-    });
+    const handleClick = () => {
+      setGreet(randomGreet());
+    };
+
+    greetText.addEventListener("click", handleClick);
+    
+    return () => {
+      greetText.removeEventListener("click", handleClick);
+    };
   }, [randomGreet]);
+
   return (
     <div id="intro-container">
-      <div id="intro-text" style={{width: greet.breakAt}}>
+      <div id="intro-text" style={{ width: greet.breakAt }}>
         {greet.name}
         <span>.</span>
       </div>
